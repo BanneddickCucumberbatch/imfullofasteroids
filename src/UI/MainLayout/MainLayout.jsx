fa0f840c9afd2698dedca7907a86e46ee36f7ac3
cucumberbatch <lovemykitty10@gmail.com>
2023-09-11 16:26:58 +0400

    almost ready

--- src/UI/MainLayout/MainLayout.jsx
+++ src/UI/MainLayout/MainLayout.jsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+import styles from "./mainLayout.module.scss";
+import { throttle } from "../../helpers/scrollThrottler";
+
 const MainLayout = ({children}) => {
+
+    const [y, setY] = useState(0);
+    
+
+    const handleScrollUpClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+
+    useEffect(() => {
+      const scrollHandler = () => {
+        setY(window.scrollY);
+      };
+
+      window.addEventListener("scroll", throttle(scrollHandler, 500));
+      return () => {
+        window.removeEventListener("scroll", throttle(scrollHandler));
+      };
+    }, []);
+
     return (
         <div className="mainContainer">
             <header className="header">
@@ -9,6 +35,7 @@ const MainLayout = ({children}) => {
             <footer className="footer">
                 <p>Все права и жители планеты защищены</p>
             </footer>
+            <div className={y > 1200 ? styles.arrowTop : styles.arrowTopHidden} onClick={handleScrollUpClick}></div>
         </div> 
      );
 }
